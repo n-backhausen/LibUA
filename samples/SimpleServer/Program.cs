f49eae2a0130f2f0ef42f1cf54e3e073a24521b7
Nick Backhausen <62666817+n-backhausen@users.noreply.github.com>
2024-07-26 18:33:14 +0200

    N backhausen add application documentation (#2)
    * Created sample for documentation
    
    * Added doc

---
+++ samples/SimpleServer/Program.cs
@@ -0,0 +1,20 @@
+﻿using LibUA.Server;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleServer
+{
+    internal class Program
+    {
+        static ILoggerFactory loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+
+        static void Main(string[] args)
+        {
+            var app = new SimpleServerApplication();
+            var server = new Master(app, 4840, 10, 30, 100, loggerFactory.CreateLogger<Master>());
+
+            server.Start();
+            Console.ReadKey();
+            server.Stop();
+        }
+    }
+}
